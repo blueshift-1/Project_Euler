f1eeda3d1c5e0a2ad684ddfb8ef0c834d5060d4f
Zachary Wilson <zachary.wilson13@gmail.com>
2016-03-29 19:17:30 -0400

    Working an reasonabley fast
    Run Time: 4.327 seconds

--- Problem_0052.py
+++ Problem_0052.py
@@ -4,29 +4,27 @@ import time
 start_time = time.time()
 
 def validateDigits(list):
-    dict = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
 
-    master = dict
+    master = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+
     for i in range(0, len(list[0])):
         master[int(list[0][i])] += 1
-
     for i in range (1, len(list)):
-        local = dict
+        local = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
 
         for j in range(0, len(list[i])):
-            local [int(list[i][j])] += 1
+            local[int(list[i][j])] += 1
 
         if local != master:
             return False
 
     return True
 
-for i in range(100,110):
+for i in range(100000,200000):
 
     vals = []
     for j in range(1,7):
         vals.append(str(i * j))
-    print (vals)
     if validateDigits(vals) == True:
         print(i)
 
